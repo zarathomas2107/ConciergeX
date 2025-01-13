8c436ef293a711a74c4419deb721d829dbe19468
Zara Thomas <zara.thomas95@gmail.com>
2025-01-13 23:32:05 +0000

    feat: Add iOS project configuration (excluding build files)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import MapboxMaps
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize MapBox
+    if let token = Bundle.main.object(forInfoDictionaryKey: "MGLMapboxAccessToken") as? String {
+        MapboxOptions.accessToken = token
+    }
+    
+    GMSServices.provideAPIKey("AIzaSyC228_kFDkb_z3PetipFzvjeDueyZKAzgA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
